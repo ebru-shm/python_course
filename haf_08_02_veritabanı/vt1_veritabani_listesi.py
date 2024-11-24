bbda36bc1344f3cf88800c690d063b9cee3616ad
ebru-shm <ebrusehmen5@gmail.com>
2024-11-24 12:36:53 +0300

    Add files via upload

---
+++ haf_08_02_veritabanı/vt1_veritabani_listesi.py
@@ -0,0 +1,24 @@
+
+# veritabanı sistemine bağlanmak için gerekli kodlar.
+import mysql.connector
+
+try:
+  vts = mysql.connector.connect(
+    host="localhost", # Veritabanı sistemi adı (instance).
+    user="root", # Veritabanı kullanıcı adı
+    password="1234" # Veritabanı sistemi(instance) şifresi
+  )
+  print("Bağlantı tamam:")
+  print(vts)
+
+  try:
+    secilen = vts.cursor()
+    secilen.execute("CREATE DATABASE pythondersleri")
+    print("Veritabanı oluşturuldu.")
+  except mysql.connector.Error as hata:
+    print(f"Veri tabanı oluşturulamadı. Hata : {hata}")
+
+
+except:
+  print("Veritabanına bağlanırken bir hata oluştu.")
+# veritabanı sistemine bağlanma sistem kurulu değilse veya herhangi bir eksik varsa hata verecektir.
