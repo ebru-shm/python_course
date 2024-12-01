ffc44301a1273b8c3d615583813c9181b97ed3b8
ebru-shm <ebrusehmen5@gmail.com>
2024-12-01 13:12:18 +0300

    Add files via upload

--- haf_09_02/va11_opencv1.py
+++ haf_09_02/va11_opencv1.py
@@ -4,9 +4,18 @@ import cv2
 vid = cv2.VideoCapture(0)
 while True:
     ret, kare = vid.read()
-    cevrilmis = cv2.rotate(kare, cv2.ROTATE_90_CLOCKWISE)
     cv2.imshow('pencere',kare)
+    cevrilmis = cv2.rotate(kare, cv2.ROTATE_90_CLOCKWISE)
     cv2.imshow('pencere1',cevrilmis)
+
+    bulanik = cv2.GaussianBlur(kare,(7,7),0)
+    cv2.imshow('pencere2',bulanik)
+
+    hsvSekli = cv2.cvtColor(kare, cv2.COLOR_BGR2HSV)
+    cv2.imshow('pencere3',hsvSekli)
+
+    sbSekli = cv2.cvtColor(kare, cv2.COLOR_BGR2GRAY)  # siyah beyaz
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
